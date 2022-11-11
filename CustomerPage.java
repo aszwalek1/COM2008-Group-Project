2d18f45949933dcb5a3d9ea49d2a970077ffc7ad
aca20sbp <sbpradhan1@sheffield.ac.uk>
2022-11-11 21:24:41 +0000

    Modified the Home page ,Customer Page and Staff Page

--- CustomerPage.java
+++ CustomerPage.java
@@ -4,23 +4,56 @@ import java.awt.*;
 public class CustomerPage {
 
     public CustomerPage() {
-        JButton back = new JButton("GO BACK");
-        JButton con = new JButton("CONTINUE");
 
         JFrame f = new JFrame("Customer");
+        f.setVisible(true);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        Container contentPane = f.getContentPane();
-        contentPane.add(back);
-        contentPane.add(con);
-        contentPane.setLayout(new FlowLayout());
+        //Panel
+        JPanel panel = new JPanel();
+        f.add(panel);
+        panel.setLayout(null);
+
+
+
+        JLabel name = new JLabel("ENTER YOUR ORDER DETAILS");
+        name.setFont(new Font("Arial",Font.BOLD,25));
+        name.setBounds(550, 200, 1000,100);
+        panel.add(name);
+
+        //label
+        JLabel order_id = new JLabel("Order ID");
+        order_id.setBounds(550,300,80,25);
+        panel.add(order_id);
+
+
+        //Text field
+        JTextField text = new JTextField(20);
+        text.setBounds(650,300,165,25);
+        panel.add(text);
+
+
+        //Button
+
+        JButton login = new JButton("LOGIN");
+        login.setBounds(750,400,80,25);
+        panel.add(login);
+
+        JButton back = new JButton("BACK");
+        back.setBounds(650,400,80,25);
+        panel.add(back);
+
+
 
         back.addActionListener(ae -> {
             f.dispose();
             new HomePage();
         });
 
+
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(500,500);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
         f.setVisible(true);
     }
 }
--- HomePage.java
+++ HomePage.java
@@ -1,27 +1,68 @@
 import javax.swing.*;
 import java.awt.*;
-public class HomePage {
-    private final JFrame f = new JFrame("BUILD A BIKE");
 
+public class HomePage {
 
     public HomePage() {
 
-        Container contentPane = f.getContentPane();
-        f.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        JButton clogin = new JButton("CUSTOMER LOGIN");
-        contentPane.add(clogin);
-        JButton slogin = new JButton("STAFF LOGIN");
-        contentPane.add(slogin);
+        JFrame f = new JFrame("BUILD A BIKE");
+        JPanel panel= new JPanel();
+        f.getContentPane();
+        panel.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(panel);
+
+
+        JLabel name = new JLabel("WELCOME TO BUILD-A-BIKE");
+        name.setFont(new Font("Arial",Font.BOLD,25));
+        name.setBounds(575, 200, 1000,100);
+        panel.add(name);
+
+
+//        ImageIcon image = new ImageIcon("bike.jpg");
+//        JLabel imageLabel = new JLabel(image);
+//        panel.add(imageLabel);
+//        imageLabel.setBounds(500,500,80,20);
+//        imageLabel.setVisible(true);
+
+
+
+
+        JButton clogin = new JButton(" CUSTOMER");
+        Dimension size = clogin.getPreferredSize();
+        clogin.setBounds(550,400 ,size.width, size.height);
+        clogin.setSize(100,50);
+        clogin.setMargin(new Insets(5,5,5,5));
+        clogin.setBackground(new Color(59, 89, 182));
+        clogin.setForeground(Color.WHITE);
+        clogin.setFont(new Font("Arial", Font.BOLD, 12));
+        panel.add(clogin);
+
+        JButton slogin = new JButton("STAFF");
+        Dimension size1 = slogin.getPreferredSize();
+        slogin.setBounds(700,400 ,size1.width, size1.height);
+        slogin.setSize(100,50);
+        slogin.setBackground(new Color(59, 89, 182));
+        slogin.setForeground(Color.WHITE);
+        slogin.setFont(new Font("Arial", Font.BOLD, 12));
+        panel.add(slogin);
+
         JButton browse = new JButton("BROWSE");
-        contentPane.add(browse);
-        contentPane.setLayout(new FlowLayout());
+        Dimension size2 = browse.getPreferredSize();
+        browse.setBounds(850,400 ,size2.width, size2.height);
+        browse.setSize(100,50);
+        browse.setBackground(new Color(59, 89, 182));
+        browse.setForeground(Color.WHITE);
+        browse.setFont(new Font("Arial", Font.BOLD, 12));
+        panel.add(browse);
+
 
         clogin.addActionListener(ae -> {
-            f.dispose();
-            new CustomerPage();
+          f.dispose();
+           new CustomerPage();
         });
 
-        slogin.addActionListener(e -> {
+       slogin.addActionListener(e -> {
             f.dispose();
             new StaffPage();
         });
@@ -31,8 +72,9 @@ public class HomePage {
             new BikePage();
         });
 
-        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
         f.setVisible(true);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
     public static void main(String[] args) {
