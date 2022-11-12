0771da192fda0bfd7a8b469d2eb2812b7d603048
aca20sbp <sbpradhan1@sheffield.ac.uk>
2022-11-12 16:32:01 +0000

    Changing button colours

--- StaffLogin.java
+++ StaffLogin.java
@@ -14,6 +14,16 @@ public class StaffLogin {
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+
+
+        JPanel logo = new JPanel();
+        f.add(logo, BorderLayout.NORTH);
+
+
+        JLabel logoName = new JLabel("BUILD-A-BIKE");
+        logoName.setFont(new Font("Arial",Font.BOLD,25));
+        logo.add(logoName);
+
         //Panel
         JPanel panel = new JPanel();
         f.add(panel);
@@ -23,40 +33,40 @@ public class StaffLogin {
 
         JLabel name = new JLabel("STAFF LOGIN");
         name.setFont(new Font("Arial",Font.BOLD,25));
-        name.setBounds(650, 200, 1000,100);
+        name.setBounds(700, 200, 1000,100);
         panel.add(name);
 
         //label
-        JLabel username = new JLabel("STAFF LOGIN");
-        username.setBounds(550,300,80,25);
+        JLabel username = new JLabel("LOGIN ID");
+        username.setBounds(650,300,80,25);
         panel.add(username);
 
         JLabel password = new JLabel("PASSWORD");
-        password.setBounds(550,350,80,25);
+        password.setBounds(650,350,80,25);
         panel.add(password);
 
 
         //Text field
         JTextField text = new JTextField(20);
-        text.setBounds(650,300,165,25);
+        text.setBounds(750,300,165,25);
         panel.add(text);
 
         //Password field
         JPasswordField p = new JPasswordField();
-        p.setBounds(650,350,165,25);
+        p.setBounds(750,350,165,25);
         panel.add(p);
 
         //Button
 
         JButton login = new JButton("LOGIN");
-        login.setBounds(750,400,80,25);
+        login.setBounds(800,400,80,25);
         login.setBackground(new Color(59, 89, 182));
         login.setForeground(Color.WHITE);
         login.setFont(new Font("Arial", Font.BOLD, 12));
         panel.add(login);
 
         JButton back = new JButton("BACK");
-        back.setBounds(650,400,80,25);
+        back.setBounds(700,400,80,25);
         back.setBackground(new Color(59, 89, 182));
         back.setForeground(Color.WHITE);
         back.setFont(new Font("Arial", Font.BOLD, 12));
